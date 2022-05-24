8e4d1ca4242d1bb18d5a05c5cbe6765ae7a25d26
Natalia Moran <58750885+nataliamoran@users.noreply.github.com>
2022-05-24 09:52:47 -0400

    Add draft tool window (#1)
    * Add draft tool window
    
    * Update window factory title and icon

---
+++ src/main/kotlin/com/github/cunla/githubactionstab/actions/GithubActionsToolWindowFactory.kt
@@ -0,0 +1,21 @@
+package com.github.cunla.githubactionstab.actions
+
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.wm.ToolWindow
+import com.intellij.openapi.wm.ToolWindowFactory
+import com.intellij.openapi.wm.ex.ToolWindowEx
+import com.intellij.ui.content.ContentFactory
+import javax.swing.JPanel
+
+
+
+class GithubActionsToolWindowFactory : ToolWindowFactory {
+
+    private val basePanel: JPanel? = null
+
+    override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) = with(toolWindow as ToolWindowEx) {
+        toolWindow.getContentManager().addContent(
+            ContentFactory.SERVICE.getInstance().createContent(basePanel, "", false)
+        )
+    }
+}
