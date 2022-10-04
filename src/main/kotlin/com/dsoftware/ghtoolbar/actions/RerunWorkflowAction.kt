b300b08aa4e56bd50e477d952e610d401cd62bf0
Daniel M <daniel.maruani@gmail.com>
2022-10-04 09:24:45 -0400

    Add icons

--- src/main/kotlin/com/dsoftware/ghtoolbar/actions/RerunWorkflowAction.kt
+++ src/main/kotlin/com/dsoftware/ghtoolbar/actions/RerunWorkflowAction.kt
@@ -1,27 +1,28 @@
 package com.dsoftware.ghtoolbar.actions
 
+import com.intellij.icons.AllIcons
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.actionSystem.DataContext
 import com.intellij.openapi.diagnostic.logger
 import com.intellij.openapi.project.DumbAwareAction
 
-class RerunWorkflowAction : DumbAwareAction("Rerun Workflow") {
+class RerunWorkflowAction : DumbAwareAction("Rerun Workflow", null, AllIcons.Actions.Rerun) {
     //todo
     override fun update(e: AnActionEvent) {
-        val data = getData(e.dataContext)
+        val data = getUrl(e.dataContext)
         e.presentation.isEnabledAndVisible = data != null
     }
 
     override fun actionPerformed(e: AnActionEvent) {
         LOG.debug("RerunWorkflowAction action")
         e.dataContext.getData(CommonDataKeys.PROJECT) ?: return
-        getData(e.dataContext)?.let {
+        getUrl(e.dataContext)?.let {
             LOG.debug("Triggering rerun ${it}")
         }
     }
 
-    private fun getData(dataContext: DataContext): String? {
+    private fun getUrl(dataContext: DataContext): String? {
         dataContext.getData(CommonDataKeys.PROJECT) ?: return null
         return dataContext.getData(ActionKeys.SELECTED_WORKFLOW_RUN)?.rerun_url
     }
--- src/main/kotlin/com/dsoftware/ghtoolbar/actions/WorkflowOpenInBrowserAction.kt
+++ src/main/kotlin/com/dsoftware/ghtoolbar/actions/WorkflowOpenInBrowserAction.kt
@@ -1,12 +1,14 @@
 package com.dsoftware.ghtoolbar.actions
 
+import com.intellij.icons.AllIcons
 import com.intellij.ide.BrowserUtil
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.actionSystem.DataContext
 import com.intellij.openapi.project.DumbAwareAction
 
-abstract class OpenInBrowserAction : DumbAwareAction("Open GitHub link in browser") {
+abstract class OpenInBrowserAction
+    : DumbAwareAction("Open GitHub Link in Browser", null, AllIcons.Xml.Browsers.Chrome) {
 
     override fun update(e: AnActionEvent) {
         val data = getData(e.dataContext)
