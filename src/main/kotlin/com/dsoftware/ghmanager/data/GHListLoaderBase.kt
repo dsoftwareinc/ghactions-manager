afa3d47f25231cdce3afdbee4be757d9a44f22cf
Daniel M <daniel.maruani@gmail.com>
2022-10-28 16:44:20 -0400

    Remove usage of GHListLoader

--- src/main/kotlin/com/dsoftware/ghmanager/data/GHListLoaderBase.kt
+++ src/main/kotlin/com/dsoftware/ghmanager/data/GHListLoaderBase.kt
@@ -4,109 +4,117 @@ package com.dsoftware.ghmanager.data
 import com.intellij.collaboration.async.CompletableFutureUtil
 import com.intellij.collaboration.async.CompletableFutureUtil.handleOnEdt
 import com.intellij.collaboration.async.CompletableFutureUtil.submitIOTask
+import com.intellij.collaboration.ui.SimpleEventListener
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.progress.ProgressIndicator
 import com.intellij.openapi.progress.ProgressManager
-import com.intellij.openapi.util.Disposer
 import com.intellij.util.EventDispatcher
-import com.intellij.collaboration.ui.SimpleEventListener
+import com.intellij.util.concurrency.annotations.RequiresEdt
 import org.jetbrains.plugins.github.pullrequest.data.GHListLoader
 import org.jetbrains.plugins.github.util.NonReusableEmptyProgressIndicator
 import java.util.concurrent.CompletableFuture
 import kotlin.properties.Delegates
 
-abstract class GHListLoaderBase<T>(protected val progressManager: ProgressManager)
-  : GHListLoader<T> {
+abstract class GHListLoaderBase<T>(
+    protected val progressManager: ProgressManager
+) : Disposable {
 
-  private var lastFuture = CompletableFuture.completedFuture(emptyList<T>())
-  private var progressIndicator = NonReusableEmptyProgressIndicator()
 
-  private val loadingStateChangeEventDispatcher = EventDispatcher.create(SimpleEventListener::class.java)
-  override var loading: Boolean by Delegates.observable(false) { _, _, _ ->
-    loadingStateChangeEventDispatcher.multicaster.eventOccurred()
-  }
+    private var lastFuture = CompletableFuture.completedFuture(emptyList<T>())
+    private var progressIndicator = NonReusableEmptyProgressIndicator()
 
-  private val errorChangeEventDispatcher = EventDispatcher.create(SimpleEventListener::class.java)
-  override var error: Throwable? by Delegates.observable<Throwable?>(null) { _, _, _ ->
-    errorChangeEventDispatcher.multicaster.eventOccurred()
-  }
+    private val loadingStateChangeEventDispatcher = EventDispatcher.create(SimpleEventListener::class.java)
 
-  protected val dataEventDispatcher = EventDispatcher.create(GHListLoader.ListDataListener::class.java)
-  override val loadedData = ArrayList<T>()
+    @get:RequiresEdt
+    var loading: Boolean by Delegates.observable(false) { _, _, _ ->
+        loadingStateChangeEventDispatcher.multicaster.eventOccurred()
+    }
 
-  override val hasLoadedItems: Boolean
-    get() = loadedData.isNotEmpty()
+    private val errorChangeEventDispatcher = EventDispatcher.create(SimpleEventListener::class.java)
 
-  override fun canLoadMore() = !loading && (error != null)
+    @get:RequiresEdt
+    var error: Throwable? by Delegates.observable<Throwable?>(null) { _, _, _ ->
+        errorChangeEventDispatcher.multicaster.eventOccurred()
+    }
 
-  override fun loadMore(update: Boolean) {
-    if (Disposer.isDisposed(this)) return
+    protected val dataEventDispatcher = EventDispatcher.create(GHListLoader.ListDataListener::class.java)
 
-    val indicator = progressIndicator
-    if (canLoadMore() || update) {
-      loading = true
-      requestLoadMore(indicator, update).handleOnEdt { list, error ->
-        if (indicator.isCanceled) return@handleOnEdt
+    @get:RequiresEdt
+    val loadedData = ArrayList<T>()
+
+    @RequiresEdt
+    open fun canLoadMore() = !loading && (error != null)
+
+    @RequiresEdt
+    fun loadMore(update: Boolean = false) {
+        val indicator = progressIndicator
+        if (canLoadMore() || update) {
+            loading = true
+            requestLoadMore(indicator, update).handleOnEdt { list, error ->
+                if (indicator.isCanceled) return@handleOnEdt
+                loading = false
+                if (error != null) {
+                    if (!CompletableFutureUtil.isCancellation(error)) this.error = error
+                } else if (list != null) {
+                    val startIdx = loadedData.size
+                    loadedData.addAll(list)
+                    dataEventDispatcher.multicaster.onDataAdded(startIdx)
+                }
+            }
+        }
+    }
+
+    private fun requestLoadMore(indicator: ProgressIndicator, update: Boolean): CompletableFuture<List<T>> {
+        lastFuture = lastFuture.thenCompose {
+            progressManager.submitIOTask(indicator) {
+                doLoadMore(indicator, update)
+            }
+        }
+        return lastFuture
+    }
+
+    protected abstract fun doLoadMore(indicator: ProgressIndicator, update: Boolean): List<T>?
+
+    fun updateData(item: T) {
+        val index = loadedData.indexOfFirst { it == item }
+        if (index >= 0) {
+            loadedData[index] = item
+            dataEventDispatcher.multicaster.onDataUpdated(index)
+        }
+    }
+
+    fun removeData(predicate: (T) -> Boolean) {
+        val (index, data) = loadedData.withIndex().find { predicate(it.value) } ?: return
+        if (index >= 0) {
+            loadedData.removeAt(index)
+            dataEventDispatcher.multicaster.onDataRemoved(data as Any)
+        }
+    }
+
+    @RequiresEdt
+    open fun reset() {
+        lastFuture = lastFuture.handle { _, _ ->
+            listOf<T>()
+        }
+        progressIndicator.cancel()
+        progressIndicator = NonReusableEmptyProgressIndicator()
+        error = null
         loading = false
-        if (error != null) {
-          if (!CompletableFutureUtil.isCancellation(error)) this.error = error
-        }
-        else if (list != null) {
-          val startIdx = loadedData.size
-          loadedData.addAll(list)
-          dataEventDispatcher.multicaster.onDataAdded(startIdx)
-        }
-      }
+        loadedData.clear()
+        dataEventDispatcher.multicaster.onAllDataRemoved()
     }
-  }
 
-  private fun requestLoadMore(indicator: ProgressIndicator, update: Boolean): CompletableFuture<List<T>> {
-    lastFuture = lastFuture.thenCompose {
-      progressManager.submitIOTask(indicator) {
-        doLoadMore(indicator, update)
-      }
-    }
-    return lastFuture
-  }
+    @RequiresEdt
+    fun addLoadingStateChangeListener(disposable: Disposable, listener: () -> Unit) =
+        SimpleEventListener.addDisposableListener(loadingStateChangeEventDispatcher, disposable, listener)
 
-  protected abstract fun doLoadMore(indicator: ProgressIndicator, update: Boolean): List<T>?
+    @RequiresEdt
+    fun addDataListener(disposable: Disposable, listener: GHListLoader.ListDataListener) =
+        dataEventDispatcher.addListener(listener, disposable)
 
-  override fun updateData(item: T) {
-    val index = loadedData.indexOfFirst { it == item }
-    if (index >= 0) {
-      loadedData[index] = item
-      dataEventDispatcher.multicaster.onDataUpdated(index)
-    }
-  }
+    @RequiresEdt
+    fun addErrorChangeListener(disposable: Disposable, listener: () -> Unit) =
+        SimpleEventListener.addDisposableListener(errorChangeEventDispatcher, disposable, listener)
 
-  override fun removeData(predicate: (T) -> Boolean) {
-    val (index, data) = loadedData.withIndex().find { predicate(it.value) } ?: return
-    if (index >= 0) {
-      loadedData.removeAt(index)
-      dataEventDispatcher.multicaster.onDataRemoved(data as Any)
-    }
-  }
-
-  override fun reset() {
-    lastFuture = lastFuture.handle { _, _ ->
-      listOf<T>()
-    }
-    progressIndicator.cancel()
-    progressIndicator = NonReusableEmptyProgressIndicator()
-    error = null
-    loading = false
-    loadedData.clear()
-    dataEventDispatcher.multicaster.onAllDataRemoved()
-  }
-
-  override fun addLoadingStateChangeListener(disposable: Disposable, listener: () -> Unit) =
-    SimpleEventListener.addDisposableListener(loadingStateChangeEventDispatcher, disposable, listener)
-
-  override fun addDataListener(disposable: Disposable, listener: GHListLoader.ListDataListener) =
-    dataEventDispatcher.addListener(listener, disposable)
-
-  override fun addErrorChangeListener(disposable: Disposable, listener: () -> Unit) =
-    SimpleEventListener.addDisposableListener(errorChangeEventDispatcher, disposable, listener)
-
-  override fun dispose() = progressIndicator.cancel()
+    override fun dispose() = progressIndicator.cancel()
 }
